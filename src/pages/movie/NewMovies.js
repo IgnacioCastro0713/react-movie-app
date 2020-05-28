6f2967db85c9640402512e2edf1dafb783992ac6
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2020-05-27 21:54:35 -0500

    add pages

---
+++ src/pages/movie/NewMovies.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function NewMovies() {
+  return (<div>
+    <h1>new movies...</h1>
+  </div>);
+}
---
+++ src/pages/movie/Popular.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Popular(){
+  return (<div>
+    <h1>Popular...</h1>
+  </div>);
+}
