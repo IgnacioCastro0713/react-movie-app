4d1c978a6803b9f58ccb63e7dfdb9f441d405d37
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2020-05-28 23:31:36 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Layout } from 'antd';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home, NewMovies, Popular, Search, Movie, Error } from './pages';
 
 function App() {
-  return (
-    <div>
 
-    </div>
+  const { Header, Content } = Layout;
+
+  return (
+    <Layout>
+      <Router>
+        <Header>
+          header...
+        </Header>
+        <Content>
+          <Switch>
+            <Route path="/" exat={ true }>
+              <Home/>
+            </Route>
+            <Route path="/new-movies" exat={ true }>
+              <NewMovies/>
+            </Route>
+            <Route path="/popular" exat={ true }>
+              <Popular/>
+            </Route>
+            <Route path="/search" exat={ true }>
+              <Search/>
+            </Route>
+            <Route path="/movie/:id" exat={ true }>
+              <Movie/>
+            </Route>
+            <Route path="*">
+              <Error/>
+            </Route>
+          </Switch>
+        </Content>
+      </Router>
+    </Layout>
   );
 }
+
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+// base
+import Home from './Home';
+import Error from './Error';
+// movie
+import Movie from "./movie/Movie";
+import NewMovies from "./movie/NewMovies";
+import Popular from "./movie/Popular";
+import Search from "./movie/Search"
+
+export {
+  Home,
+  Error,
+  Movie,
+  NewMovies,
+  Popular,
+  Search
+}
+
