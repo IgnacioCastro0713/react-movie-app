4a4ab2cabe515f32004397d0b659bbe4dbb33515
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2020-07-18 22:26:48 -0500

    new movies section added

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as MenuTop } from './MenuTop';
 export { default as Slider } from './Slider';
-export { default  as Loading} from './Loading';
-export { default  as List} from './List';
+export { default as Loading } from './Loading';
+export { default as List } from './List';
 export { default as Footer } from './Footer';
 export { default as ModalVideo } from './ModalVideo';
+export { default as MovieCatalog } from './MovieCatalog';
--- src/pages/NewMovies/NewMovies.js
+++ src/pages/NewMovies/NewMovies.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Col, Row } from "antd/lib";
+import { API_KEY, API_URL } from '../../config/env';
+import { Loading, MovieCatalog } from '../../components';
 
 export default function NewMovies() {
-  return (<div>
-    <h1>new movies...</h1>
-  </div>);
+  const [movieList, setMovieList] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`${ API_URL }/movie/now_playing?api_key=${ API_KEY }&language=es-ES&page=${ page }`);
+      const movies = await response.json();
+      setMovieList(movies);
+    })();
+  }, [page]);
+
+  return (
+    <Row>
+      <Col span={ 24 } style={ { textAlign: "center", marginTop: 25 } }>
+        <h1 style={ { fontSize: 35, fontWeight: "bold" } }>
+          Latest releases
+        </h1>
+      </Col>
+      { movieList.results ? (
+        <Row>
+
+          <MovieCatalog movies={ movieList }/>
+
+        </Row>
+      ) : (
+        <Col span={ 24 }>
+          <Loading/>
+        </Col>
+      ) }
+    </Row>
+  );
 }
