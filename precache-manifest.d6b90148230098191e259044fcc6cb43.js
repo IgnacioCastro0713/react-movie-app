9cdede6841a719a4fcb2de9fedeb71a46b00cbf3
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2020-07-20 00:03:26 -0500

    Updates

---
+++ precache-manifest.d6b90148230098191e259044fcc6cb43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729211ed5865bb584f7ab9ca05066fc6",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "6cb3f50a9759a24f9a9d",
+    "url": "/react-movie-app/static/css/2.19e0b811.chunk.css"
+  },
+  {
+    "revision": "f0203a0d953287229522",
+    "url": "/react-movie-app/static/css/main.7e0b7e41.chunk.css"
+  },
+  {
+    "revision": "6cb3f50a9759a24f9a9d",
+    "url": "/react-movie-app/static/js/2.15ecebc0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-movie-app/static/js/2.15ecebc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0203a0d953287229522",
+    "url": "/react-movie-app/static/js/main.99d8e8c2.chunk.js"
+  },
+  {
+    "revision": "882064c2b9389aaf998a",
+    "url": "/react-movie-app/static/js/runtime-main.c74879a0.js"
+  },
+  {
+    "revision": "cb90c5bda3dbe4e16f7607c0c5213f3a",
+    "url": "/react-movie-app/static/media/original.cb90c5bd.svg"
+  }
+]);
